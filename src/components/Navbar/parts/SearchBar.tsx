775121baef8400d7739013c955b540f142258f09
Michal_Parchanowicz <michal.parchanowicz@gmail.com>
2024-05-22 19:06:01 +0200

    feat: improving search bar

--- src/components/Navbar/parts/SearchBar.tsx
+++ src/components/Navbar/parts/SearchBar.tsx
@@ -3,33 +3,75 @@
 import { cn } from "@/libs/twMerge.lib";
 import { Category } from "@/types/models/category.types";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function SearchBar({ categories }: { categories: Category[] }) {
   const [categoryOpened, setCategoryOpened] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState("All categories");
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setCategoryOpened(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="navbar-search-bar">
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search..." ref={inputRef} />
       <div className="separator" />
-      <div className="select-container" onClick={() => setCategoryOpened(!categoryOpened)}>
+      <div
+        className={cn("select-container", {
+          active: categoryOpened
+        })}
+        onClick={() => setCategoryOpened(!categoryOpened)}
+      >
         {selectedCategory}
+        <Image
+          src="/assets/icons/arrow.svg"
+          alt="Arrow logo"
+          width={16}
+          height={16}
+          className="arrow-icon"
+        />
       </div>
       <div
         className={cn("category-dropdown", {
           active: categoryOpened
         })}
+        ref={dropdownRef}
       >
         <ul>
-          <li onClick={() => setSelectedCategory("All categories")}>All categories</li>
+          <li
+            onClick={() => {
+              setSelectedCategory("All categories");
+              setCategoryOpened(!categoryOpened);
+              inputRef.current?.focus();
+            }}
+            className={cn("category-item", {
+              selected: selectedCategory === "All categories"
+            })}
+          >
+            All categories
+          </li>
           {categories.map((category: Category) => (
             <li
               key={category._id}
               onClick={() => {
                 setSelectedCategory(category.name);
                 setCategoryOpened(!categoryOpened);
+                inputRef.current?.focus();
               }}
+              className={cn("category-item", {
+                selected: selectedCategory === category.name
+              })}
             >
               {category.name}
             </li>
